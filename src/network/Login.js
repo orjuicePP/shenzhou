9b4d25dcc9348e19b7faa7adf39f678b4789fc4c
60rzvvbj <1808078515@qq.com>
2021-09-20 21:13:36 +0800

    add Login.js

---
+++ src/network/Login.js
@@ -0,0 +1,30 @@
+import {
+    request
+} from './request.js';
+
+// 登录
+export function login(data) {
+    return request({
+        method: 'POST',
+        url: '/login',
+        params: {
+            username: data.username,
+            password: data.password
+        }
+    });
+};
+
+// 修改密码
+export function changePwd(data) {
+    return request({
+        method: 'POST',
+        url: '/changePwd',
+        headers: {
+            token: data.token
+        },
+        params: {
+            oldPassword: data.oldPwd,
+            newPassword: data.newPwd
+        }
+    });
+};
