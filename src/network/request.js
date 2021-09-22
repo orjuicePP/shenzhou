b57a1a1bd97f26660158f3f54b894fd07706cd45
60rzvvbj <1808078515@qq.com>
2021-09-22 15:46:31 +0800

    add ;

--- src/network/request.js
+++ src/network/request.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export const status = 'build';
 
-export function request (config) {
-  let instance;
-  instance = axios.create({
-    baseURL: '/api',
-    timeout: 5000,
-  })
-  return instance(config);
+export function request(config) {
+    let instance;
+    instance = axios.create({
+        baseURL: '/api',
+        timeout: 5000,
+    });
+    return instance(config);
 };
