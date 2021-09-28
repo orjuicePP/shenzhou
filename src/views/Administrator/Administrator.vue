5d91fddd958c91a93f4c2382d7807807ed21c1da
60rzvvbj <1808078515@qq.com>
2021-09-28 22:43:39 +0800

    add admin page body

--- src/views/Administrator/Administrator.vue
+++ src/views/Administrator/Administrator.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="admin">
         <Header></Header>
-        <div class="core">管理员页面</div>
+        <div class="core">
+            <div class="tabBar">
+                <ul>
+                    <li class="now">导游申请</li>
+                    <li>礼品兑换申请</li>
+                </ul>
+            </div>
+            <div class="content"></div>
+        </div>
     </div>
 </template>
 
@@ -22,6 +30,47 @@ export default {
 .admin {
     width: 100%;
     height: 100%;
-    background-color: #eee;
+    background-color: #e3e3e3;
+}
+
+.core {
+    width: var(--baseWidth);
+    margin: 50px auto 0px;
+}
+
+.core .tabBar {
+    --tabHeight: 50px;
+    height: var(--tabHeight);
+}
+
+.core ul {
+    height: 100%;
+}
+
+.core ul li {
+    float: left;
+    height: 100%;
+    padding: 0px 20px;
+    line-height: var(--tabHeight);
+    border-radius: 20px 20px 0px 0px;
+    background-color: #f3f3f3;
+    cursor: pointer;
+}
+
+.core ul li:hover {
+    background-color: #fafafa;
+}
+
+.core ul .now {
+    background-color: #fff;
+}
+
+.core ul .now:hover {
+    background-color: #fff;
+}
+
+.core .content {
+    height: 500px;
+    background-color: #fff;
 }
 </style>
