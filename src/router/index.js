16fe5f11c86e48fe4c6b795aea4e8c4246d1978c
60rzvvbj <1808078515@qq.com>
2021-10-02 22:00:10 +0800

    init article page

--- src/router/index.js
+++ src/router/index.js
@@ -3,67 +3,68 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-const routes = [{
-        path: '/',
-        name: 'loginTest',
-        component: () => import('views/loginTest/LoginTest.vue'),
-    },
-    {
-        path: '/loginTest',
-        redirect: '/',
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: () => import('views/loginTest/Register.vue'),
-    },
-    {
-        path: '/home',
-        name: 'home',
-        component: () => import('views/home/Home.vue'),
-    },
-    {
-        path: '/guide',
-        name: 'guide',
-        component: () => import('views/guide/Guide.vue'),
-    },
-    {
-        path: '/articles',
-        name: 'articles',
-        component: () => import('views/articles/Articles.vue'),
-    },
-    {
-        path: '/administrator',
-        name: 'administrator',
-        component: () => import('views/administrator/Administrator.vue'),
-    },
-    {
-        path: '/shop',
-        name: 'shop',
-        component: () => import('views/shop/Shop.vue'),
-    },
+const routes = [
+	{
+		path: '/',
+		name: 'loginTest',
+		component: () => import('views/loginTest/LoginTest.vue'),
+	},
+	{
+		path: '/loginTest',
+		redirect: '/',
+	},
+	{
+		path: '/register',
+		name: 'register',
+		component: () => import('views/loginTest/Register.vue'),
+	},
+	{
+		path: '/home',
+		name: 'home',
+		component: () => import('views/home/Home.vue'),
+	},
+	{
+		path: '/guide',
+		name: 'guide',
+		component: () => import('views/guide/Guide.vue'),
+	},
+	{
+		path: '/article',
+		name: 'article',
+		component: () => import('views/article/Article.vue'),
+	},
+	{
+		path: '/administrator',
+		name: 'administrator',
+		component: () => import('views/administrator/Administrator.vue'),
+	},
+	{
+		path: '/shop',
+		name: 'shop',
+		component: () => import('views/shop/Shop.vue'),
+	},
 ];
 
 const router = new VueRouter({
-    mode: 'history',
-    routes,
+	mode: 'history',
+	routes,
 });
 
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-    // to将要访问的路径
-    // from代表从哪个路径的跳转而来
-    // next是一个函数，表示放行
-    // next() 放行 next('/login')强制跳转
+	// to将要访问的路径
+	// from代表从哪个路径的跳转而来
+	// next是一个函数，表示放行
+	// next() 放行 next('/login')强制跳转
 
-    // if (to.path === '/login') {
-    //     return next();
-    // }
-    // // 获取token
-    // const tokenStr = tool.getCookie('token');
-    // // 强制跳转
-    // if (!tokenStr) return next('/login');
-    next();
+	// if (to.path === '/login') {
+	//     return next();
+	// }
+	// // 获取token
+	// const tokenStr = tool.getCookie('token');
+	// // 强制跳转
+	// if (!tokenStr) return next('/login');
+	next();
 });
 
 export default router;
---
+++ src/views/article/Article.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="ArticleRoot">
+        <Header></Header>
+        <div class="main">
+            <div class="left">
+                <div class="title"></div>
+                <div class="content"></div>
+                <div class="photo"></div>
+                <div class="time"></div>
+                <div class="thumb"></div>
+            </div>
+            <div class="right">
+                <div class="authorInfo"></div>
+                <div class="name"></div>
+                <div class="province"></div>
+                <div class="otherArticle">
+                    <p>猜你喜欢</p>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from 'components/content/Header.vue';
+export default {
+    name: 'Article',
+    data() {
+        return {};
+    },
+    components: {
+        Header,
+    }
+};
+</script>
+
+<style>
+.main {
+    width: var(--baseWidth);
+    height: 600px;
+    margin: 30px auto;
+}
+
+.main > div {
+    height: 100%;
+    border-radius: 10px;
+    background-color: #fff;
+}
+
+.main > .left {
+    float: left;
+    width: 840px;
+}
+
+.main > .right {
+    float: right;
+    width: 360px;
+}
+</style>
